91fa1f7e6864036aeeff9f160e49eb3c27fede93
harm bruins <harmbruins@MacBook-Pro.fritz.box>
2020-07-17 09:02:28 +0200

    Aanpassingen

--- src/pages/apps/implementations/dataset-management/index.js
+++ src/pages/apps/implementations/dataset-management/index.js
@@ -1,56 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Helmet } from 'react-helmet'
+import { ReactSortable } from 'react-sortablejs'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { ReactSortable } from 'react-sortablejs'
-import getDataFromAPIS from '../../../../services/spring.boot.service.datasets'
-import style from './style.module.scss'
+import { Button } from 'antd'
 import WorkItemModal from '../../../ui-kits/antd/implementations/modal'
+import style from './style.module.scss'
 
-const mapStateToProps = ({ dispatch }) => ({
-  dispatch,
+const mapStateToProps = ({ service }) => ({
+  projects: service.projects,
 })
 
-const useFetch = id => {
-  const [toDoItems, setToDoItems] = useState([])
-  const [blItems, setBlItems] = useState([])
-  const getDataFromAPI = async () => {
-    try {
-      const result = await getDataFromAPIS(id)
-      const bg = result.filter(function a(e) {
-        return e.datasetcontext.status === 'BACKLOG'
-      })
-      const td = result.filter(function b(e) {
-        return e.datasetcontext.status === 'TODO'
-      })
-      setToDoItems([...td])
-      setBlItems([...bg])
-    } catch (e) {
-      console.log('shit!')
-      console.log(e)
-    }
-  }
-  useEffect(() => {
-    getDataFromAPI()
-  }, [id])
-  return { blItems, toDoItems }
-}
+const ExtraAppsJiraAgileBoard = ({ projects }) => {
+  const [data, setData] = useState(projects)
 
-const ExtraAppsJiraAgileBoard = () => {
-  const id = sessionStorage.getItem('dataSetId')
-  const apiEndPoint = id
-  const userFetchResponse = useFetch(apiEndPoint)
-
-  // const [data,setData] = useState([])
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     return getDataFromAPI(sessionStorage.getItem('dataSetId'))
-  //   }
-  //   fetchData().then(r => setData(r));
-  // }, [sessionStorage.getItem('dataSetId')]); // Or [] if effect doesn't need props or state
-  // console.log(data)
-  //
   const agileItems = {
     name: 'Button',
     description: 'To trigger an operation.',
@@ -58,95 +21,143 @@ const ExtraAppsJiraAgileBoard = () => {
     component: <WorkItemModal datasetId={1} />,
   }
 
+  const dataSetObject = {
+    id: 1,
+    projectname: 'string',
+    description: 'string',
+    lead: 'string',
+    dataset: [
+      {
+        datasetcontext: {
+          blocked: true,
+          flag: true,
+          owner: 'string',
+          position: 0,
+          selected: true,
+          status: 'BACKLOG',
+        },
+        name: 'string',
+        path: 'string',
+        projectId: 0,
+        selector: 'string',
+        type: 'string',
+        id: 1,
+      },
+    ],
+  }
+
+  const add = () => {
+    setData([...data, dataSetObject])
+    console.log(JSON.stringify(data))
+  }
+
+  const [backlogItems, setBacklogItems] = useState(
+    data
+      .find(r => r.id === 1)
+      .dataset.map(item => {
+        console.log(item)
+        if (item.datasetcontext.status === 'BACKLOG') {
+          return (
+            <div className={style.card} key={item.id}>
+              <div className={style.content}>
+                <div className={`${style.flag} bg-primary`} />
+                <div className="d-flex flex-wrap-reverse align-items-center">
+                  <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.name}</h5>
+                  <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.id}</h5>
+                </div>
+                <div className="text-gray-5 mb-2">Description: {item.description}</div>
+                <div>{agileItems.component}</div>
+              </div>
+            </div>
+          )
+        }
+        return <div>nothing found</div>
+      }),
+  )
+
+  const [todoItems, setTodoItems] = useState(
+    data
+      .find(r => r.id === 1)
+      .dataset.map(item => {
+        console.log(item)
+        if (item.datasetcontext.status === 'TODO') {
+          return (
+            <div className={style.card} key={item.id}>
+              <div className={style.content}>
+                <div className={`${style.flag} bg-primary`} />
+                <div className="d-flex flex-wrap-reverse align-items-center">
+                  <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.name}</h5>
+                  <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.id}</h5>
+                </div>
+                <div className="text-gray-5 mb-2">Description: {item.description}</div>
+              </div>
+            </div>
+          )
+        }
+        return <div>nothing found</div>
+      }),
+  )
+
+  const [inprogressItems, setInprogressItems] = useState(
+    data
+      .find(r => r.id === 1)
+      .dataset.map(item => {
+        console.log(item)
+        if (item.datasetcontext.status === 'INPROGRESS') {
+          return (
+            <div className={style.card} key={item.id}>
+              <div className={style.content}>
+                <div className={`${style.flag} bg-primary`} />
+                <div className="d-flex flex-wrap-reverse align-items-center">
+                  <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.name}</h5>
+                  <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.id}</h5>
+                </div>
+                <div className="text-gray-5 mb-2">Description: {item.description}</div>
+              </div>
+            </div>
+          )
+        }
+        return <div>nothing found</div>
+      }),
+  )
+
+  const [completed, setCompleted] = useState(
+    data
+      .find(r => r.id === 1)
+      .dataset.map(item => {
+        console.log(item)
+        if (item.datasetcontext.status === 'COMPLETED') {
+          return (
+            <div className={style.card} key={item.id}>
+              <div className={style.content}>
+                <div className={`${style.flag} bg-primary`} />
+                <div className="d-flex flex-wrap-reverse align-items-center">
+                  <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.name}</h5>
+                  <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.id}</h5>
+                </div>
+                <div className="text-gray-5 mb-2">Description: {item.description}</div>
+              </div>
+            </div>
+          )
+        }
+        return <div>nothing found</div>
+      }),
+  )
+
   const update = async event => {
     const getIdFromTitle = event.item.innerText.split('ID: ')[1].split('Description')[0]
     console.log(getIdFromTitle)
   }
 
-  const [backlogItems, setBacklogItems] = useState(
-    userFetchResponse.blItems
-      .filter(item => item.datasetcontext.status === 'BACKLOG')
-      .map(item => {
-        console.log(item.length)
-        return (
-          <div className={style.card} key={item.id.toString()}>
-            <div className={style.content}>
-              <div className={`${style.flag} bg-primary`} />
-              <div className="d-flex flex-wrap-reverse align-items-center">
-                <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.description}</h5>
-                <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.description}</h5>
-              </div>
-              <div className="text-gray-5 mb-2">Description: {item.description}</div>
-              <div>{agileItems.component}</div>
-            </div>
-          </div>
-        )
-      }),
-  )
-
-  // const [todoItems, setTodoItems] = useState(
-  //   data.filter(item => item.datasetcontext.status === 'ToDo')
-  //     .map(item => {
-  //       return (
-  //         <div className={style.card} key={item.id.toString()}>
-  //           <div className={style.content}>
-  //             <div className={`${style.flag} bg-primary`} />
-  //             <div className="d-flex flex-wrap-reverse align-items-center">
-  //               <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.name}</h5>
-  //               <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.id}</h5>
-  //             </div>
-  //             <div className="text-gray-5 mb-2">Description: {item.description}</div>
-  //             <div>{agileItems.component}</div>
-  //           </div>
-  //         </div>
-  //       )
-  //     }),
-  // )
-  //
-  // const [inprogressItems, setInprogressItems] = useState(
-  //   data.filter(item => item.datasetcontext.status === 'InProgress')
-  //     .map(item => {
-  //       return (
-  //         <div className={style.card} key={item.id.toString()}>
-  //           <div className={style.content}>
-  //             <div className={`${style.flag} bg-primary`} />
-  //             <div className="d-flex flex-wrap-reverse align-items-center">
-  //               <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.name}</h5>
-  //               <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.id}</h5>
-  //             </div>
-  //             <div className="text-gray-5 mb-2">Description: {item.description}</div>
-  //             <div>{agileItems.component}</div>
-  //           </div>
-  //         </div>
-  //       )
-  //     }),
-  // )
-  //
-  // const [completed, setCompleted] = useState(
-  //   data.filter(item => item.datasetcontext.status === 'Done')
-  //     .map(item => {
-  //       return (
-  //         <div className={style.card} key={item.id.toString()}>
-  //           <div className={style.content}>
-  //             <div className={`${style.flag} bg-primary`} />
-  //             <div className="d-flex flex-wrap-reverse align-items-center">
-  //               <h5 className="text-dark font-size-18 mt-2 mr-auto">{item.name}</h5>
-  //               <h5 className="text-dark font-size-8 mt-2 mr-auto">ID: {item.id}</h5>
-  //             </div>
-  //             <div className="text-gray-5 mb-2">Description: {item.description}</div>
-  //             <div>{agileItems.component}</div>
-  //           </div>
-  //         </div>
-  //       )
-  //     }),
-  // )
-
   const componentContent = () => {
-    return userFetchResponse.blItems.length ? (
+    return (
       <div>
         <Helmet title="Jira Agile Board" />
         <div className="d-flex align-items-center mb-4">
           <div className="kit__utils__avatarGroup mr-4 flex-shrink-0" />
+          <Button onClick={() => add()} type="primary" className="mb-1 mr-1">
+            + Add
+          </Button>
         </div>
         <div className="row">
           <div className="col-lg-3 col-md-6">
@@ -164,12 +175,56 @@ const ExtraAppsJiraAgileBoard = () => {
               </ReactSortable>
             </div>
           </div>
+          <div className="col-lg-3 col-md-6">
+            <div className="card bg-light py-3 px-2">
+              <h3 className="font-weight-bold text-dark font-size-18 mb-3">To Do</h3>
+              <ReactSortable
+                group="issues"
+                style={{ minHeight: 500 }}
+                list={todoItems}
+                setList={setTodoItems}
+                key="id"
+                onChange={item => update(item)}
+              >
+                {todoItems.map(item => item)}
+              </ReactSortable>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div className="card bg-light py-3 px-2">
+              <h3 className="font-weight-bold text-dark font-size-18 mb-3">In Progress</h3>
+              <ReactSortable
+                group="issues"
+                style={{ minHeight: 500 }}
+                list={inprogressItems}
+                setList={setInprogressItems}
+                key="id"
+                onChange={item => update(item)}
+              >
+                {inprogressItems.map(item => item)}
+              </ReactSortable>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div className="card bg-light py-3 px-2">
+              <h3 className="font-weight-bold text-dark font-size-18 mb-3">Completed</h3>
+              <ReactSortable
+                group="issues"
+                style={{ minHeight: 500 }}
+                list={completed}
+                setList={setCompleted}
+                onChange={item => update(item)}
+                key="id"
+              >
+                {completed.map(item => item)}
+              </ReactSortable>
+            </div>
+          </div>
         </div>
       </div>
-    ) : (
-      <span>Loading wells...</span>
     )
   }
+
   return <div>{componentContent()}</div>
 }
 
--- src/services/spring.boot.service.datasets.js
+++ src/services/spring.boot.service.datasets.js
@@ -1,18 +1,19 @@
 import { notification } from 'antd'
 
 export default async function getDataSetData(id) {
-  const datam = await fetch(`http://localhost:9009/v1/project/getbyid/${id}/dataset`, {
+  return fetch(`http://localhost:9009/v1/project/getbyid/${id}/dataset`, {
     headers: {
       mode: 'cors',
       Accept: 'application/json',
       'Content-Type': 'application/json',
       Authorization: `Bearer ${sessionStorage.getItem('token')}`,
     },
-  }).catch(error =>
-    notification.warning({
-      message: error.code,
-      description: error.message,
-    }),
-  )
-  return datam.json()
+  })
+    .then(res => res.clone().json())
+    .catch(error =>
+      notification.warning({
+        message: error.code,
+        description: error.message,
+      }),
+    )
 }
